5dab3b8486b800ece28a26e78a15dbaf1c3cdfdd
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-07-21 01:55:04 -0600

    servicio htpps

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const { Server } = require("socket.io");
 const cors = require("cors");
@@ -45,6 +46,85 @@ io.on("connection", (socket) => {
     const { id_user, vrms, irms, kwh } = data;
 
     // Aquí puedes realizar cualquier operación con los datos recibidos
+    io.to(id_user).emit("datas", {
+      id_user,
+      vrms,
+      irms,
+      kwh,
+    });
+  });
+
+  socket.on("notification-alert", (data) => {
+    console.log("Notificación recibida:", data);
+    const { id_user, tipo, data: message } = data;
+
+    io.to(id_user).emit("notification-alerts", {
+      id_user,
+      tipo,
+      message,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Usuario desconectado:", socket.id_user);
+  });
+});
+*/
+const express = require("express");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+
+require("dotenv").config();
+const { verifyToken } = require("./auth.middleware");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+const optionsHTTPS = {
+  key: fs.readFileSync(String(process.env.RUTA_KEY)),
+  cert: fs.readFileSync(String(process.env.RUTA_CERTIFICADO)),
+};
+
+const server = https.createServer(credentials, app);
+
+server.listen(port, () => {
+  console.log(`API corriendo en el puerto ${port}`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.use((socket, next) => {
+  verifyToken(socket, next);
+});
+
+io.use((socket, next) => {
+  const id_user = socket.handshake.query.id_user;
+
+  if (!id_user) {
+    return next(new Error("Falta el id_user"));
+  }
+  socket.id_user = id_user;
+  next();
+});
+
+io.on("connection", (socket) => {
+  console.log("Nuevo usuario conectado:", socket.id_user);
+
+  socket.join(socket.id_user);
+
+  socket.on("data", (data) => {
+    console.log("Datos recibidos:", data);
+    const { id_user, vrms, irms, kwh } = data;
+
     io.to(id_user).emit("datas", {
       id_user,
       vrms,
